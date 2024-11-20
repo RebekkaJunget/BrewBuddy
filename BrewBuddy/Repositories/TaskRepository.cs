d2d994def86dacff5ff77ed3434d781ec52fc8ea
Rebekka Rendow Junget <rej003@edu.zealand.dk>
2024-11-20 22:18:46 +0100

    taskrep

---
+++ BrewBuddy/Repositories/TaskRepository.cs
@@ -0,0 +1,46 @@
+﻿using BrewBuddy.Interface;
+using BrewBuddy.Models;
+
+namespace BrewBuddy.Repositories
+{
+    public class TaskRepository : IRepository<Task>
+    {
+        private readonly BrewBuddyContext _context;
+
+        public TaskRepository(BrewBuddyContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Models.Task task)
+        {
+            _context.Tasks.Add(task); //her kan vi tilføje en ny task
+            _context.SaveChanges(); //denne er vigtig for det er den der sørger for at gemme den nye task 
+        }
+
+        public void Delete(int Id)
+        {
+            // Find the task by its ID
+            var task = _context.Tasks.Find(Id);
+
+            // If the task exists, remove i
+            if (task != null)
+            {
+                _context.Tasks.Remove(task);
+                _context.SaveChanges(); // Save changes to commit the deletion
+            }
+        }
+
+        public List<Models.Task> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Models.Task entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    
+}
